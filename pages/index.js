716b855a9f5c8158a62e6fa8dddd5e3f5e621002
Bia Coelho <biacoelho@pm.me>
2021-01-26 02:48:27 -0300

    add: aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Qual notícia é verdadeira?</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Teste os seus conhecimentos gerais!</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Dá uma olhada nesses quizes incríveis que o pessoal da Imersão React da Alura fez:</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/biacoelho"/>
+    </QuizBackground>
+  );
 }
